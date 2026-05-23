b80eb66298c777e3ce02b3c65006abc04f119ffb
t10767 <t10767@users.noreply.github.com>
2026-05-23 23:10:55 +0700

    Thêm API và logic tự động tạo thông báo

--- data/database.sql
+++ data/database.sql
@@ -12,3 +12,21 @@ CREATE TABLE users (
 );
 
 -- MySQL workberch -- 
+USE defaultdb;
+
+CREATE TABLE notifications (
+    id INT NOT NULL AUTO_INCREMENT,
+    user_id_nhan INT NOT NULL,
+    content TEXT NOT NULL,
+    link VARCHAR(255) DEFAULT NULL,
+    is_read TINYINT(1) DEFAULT 0,
+    created_at TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (id),
+    KEY user_id_nhan (user_id_nhan),
+
+    CONSTRAINT notifications_ibfk_1
+        FOREIGN KEY (user_id_nhan)
+        REFERENCES users (id)
+        ON DELETE CASCADE
+);
